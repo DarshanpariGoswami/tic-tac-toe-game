f1937254c16b5af286f7294e65ac36d6551bb4cf
DarshanpariGoswami <124126426+DarshanpariGoswami@users.noreply.github.com>
2023-03-04 17:53:04 +0530

    Add files via upload

---
+++ demo.js
@@ -0,0 +1,23 @@
+var xvalue = "x";
+var ovalue = "o";
+
+//var xclick = document.getElementById("player_x");
+//xclick.addEventListener("click", xevent);
+function xevent()
+{
+var xbutton=documen.getElementById("palyer_x");
+ if(xbutton.addEventListener("click", xevent())
+{
+documen.getElementById("abc").innerHTML="xvalue";
+}
+)
+}
+
+function xevent() {
+  // var node = xvalue.lastChild;
+  // var clone = node.cloneNode(true);
+  //document.getElementsByClassName("box1").appendChild(clone);
+  
+
+}
+if()
